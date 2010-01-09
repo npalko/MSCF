233ffdd8dd53e477e1b84c80c423f61ca4710462
Nicholas Palko <npalko@tepper.cmu.edu>
2010-01-08 19:08:40 -0500

    clarified intermediate values; fixed compounding bug when nCompound != 2

--- bondprice.m
+++ bondprice.m
@@ -1,26 +1,24 @@
+function price = bondprice(face, yield, maturity, coupon)
 %{
-Copyright (c) 2009, npalko@tepper.cmu.edu. All rights reserved.
-
-
-
 Typical usage:
-    
+
+    price = bondprice(face, yield, maturity, coupon);
     price = bondprice(100,.10,8,.05);
     fsolve(@(y) 119.23 - bondprice(100,y,8,.05));
-        
-
+     
 %}
-function price = bondprice(face, yield, maturity, coupon)
 
-
-    nCompound = 2;  % semi-annual compounding convention
-
+    nCompound = 2;  % number of compounds per year
+    nCashflows = nCompound * maturity;
+    
     couponPayment = coupon/nCompound*face;
-    cashflows = repmat(couponPayment, 1, nCompound*maturity);
+    cashflows = repmat(couponPayment, 1, nCashflows);
     cashflows(end) = cashflows(end) + face;
     
-    discount = repmat(1/(1+yield/2),1, nCompound*maturity);
-    discount = discount.^(1:nCompound*maturity);
+    singlePeriodDiscount = 1/(1+yield/nCompound);
+    periodCompound = (1:nCashflows);
+    discount = repmat(singlePeriodDiscount, 1, nCashflows);
+    discount = discount.^periodCompound;
     
     price = sum(cashflows .* discount);
 
