35a9f2090d9e4250cd156e8749c1a931888e78ce
Nicholas Palko <npalko@tepper.cmu.edu>
2010-01-11 07:41:45 -0500

    corrected usage of fsolve()

--- tutorial/tutorial2.m
+++ tutorial/tutorial2.m
@@ -1,9 +1,36 @@
 %{
 TUTORIAL CONFIGURATION
+* make sure you have your MATLAB path set to include
+    C:\Users\Nicholas Palko\Documents\Development\MSCF    
+    C:\Users\Nicholas Palko\Documents\Development\Cloud\MATLAB\cloud
+    
 
-spaces
-don't write past 80 lines
 
+STYLE:
+
+* spaces instead of tabs: other people use different tab settings; spaces
+  guarentee consistency
+* don't write past 80 lines (use ...): this helps when you want to look at
+  two pages of code side by side; we'll visit such situations when using
+  source control later
+
+COMMENTS:
+
+Zen of commenting:
+
+        Your code should be clear enough to tell someone HOW
+        you are doing something; comments should tell them
+        WHY the code exists
+
+GOOD CODING GUIDELINES:
+
+1. use intermediate variables with specific names to explain your thought
+   process
+2. don't hardcode constants (i.e. no random numbers showing up in code)
+3. code is written a few times, and then read hundreds of times. make 
+   make your code readable!!
+4. optimization is the very last thing you ever do! don't write code to use
+   obscure tricks to 'make things go faster'. 
 %}
 
 
@@ -29,8 +56,8 @@ f3 = @(y) bondprice(100,y,10,.04);
 %               ref: http://knowyourmeme.com/memes/xzibit-yo-dawg
 %
 % Anonymous functions are 1st class objects (a good interview question).
-% This means that functions can be passed around like any other object in
-% MATLAB (like a matrix, string, cell array, etc). This functionality 
+% This means that anonymous functions can be passed around like any other
+% object in MATLAB (like a matrix, string, cell array, etc). This 
 % allows us to ENCAPSULATE and RESUSE our hard work
 
 gbm = @(S,t) process.geometricbrownianmotion(S,t,.1,.8);
@@ -92,24 +119,20 @@ legend('90','100','110','location','NorthEast');
 %{
 DEBUGGING: BEST WAY TO ANSWER 'WHAT THE HELL IS GOING ON?!'
 
+
+Let's revisit bondprice.m
+
+>> edit bondprice.m
+
+1. set breakpoint in bondprice
+2. Observe that the stack frame changes. Workspace variables are no
+   longer local. Observe change in prompt.
+
+dbcont: continue until next breakpoint
+dbquit: give up
+dbclear all: clear all breakpoints
+
 %}
-
-% 1. set breakpoint in bondprice
-% 2. Observe that the stack frame changes. Workspace variables are no
-%    longer local. Observe change in prompt
-
-% dbcont: continue until next breakpoint
-% dbquit: give up
-% dbclear all: clear all breakpoints
-
-% Good coding guidelines:
-% 1. use intermediate variables with specific names to explain your thought
-%    process
-% 2. don't hardcode constants (i.e. no random numbers showing up in code)
-% 3. code is written a few times, and then read hundreds of times. make 
-%    make your code readable!!
-
-
 
 
 %{
@@ -117,20 +140,47 @@ DOWNLOADING MARKET DATA
 
 %}
 
+portfolio.ticker = {
+    'SPY'
+    'XLU' 
+    'XLK' 	
+    'XLF' 	
+    'XLE' 
+    'XLY'
+    'XLV'
+    };
+portfolio.description = {
+    'S&P 500 (ETF)'
+    'Utilities SPDR (ETF)'
+    'Technology SPDR (ETF)'
+    'Financial Select Sector (ETF)'
+    'Energy Select Sector SPDR (ETF)'
+    'Consumer Discretionary SPDR (ETF)'
+    'Health Care SPDR (ETF)'
+    };
+endDate = today - 1;
+startDate = endDate-500;
 
+f = marketdata.yahoo();
+f.debug = true;
 
-%{
-SEND EMAIL
-http://stackoverflow.com/questions/46663/how-do-you-send-email-from-a-java-app-using-gmail
+% determine how big we need to make our preallocation
 
-%}
+[ignore dates] = f.getData(portfolio.ticker{1}, startDate, endDate);
+nDate = numel(dates);
+nSecurity = numel(portfolio.ticker);
+close = zeros(nDate, nSecurity);
+volume = zeros(nDate, nSecurity);
 
+for i=1:nSecurity
+   [download ignore] = f.getData(portfolio.ticker{i}, startDate, endDate);
+   close(:,i) = download.close;
+   volume(:,i) = download.volume;
+end
 
-
-
-
-
-
+plot(dates,close);
+datetick('x','mmm-yy');
+legend(portfolio.ticker);
 
 
 %{
@@ -139,4 +189,32 @@ TIME SERIES BASICS
 * all time series matricies indexed as A(time,security)
 * A(1,:) is the first temperal observation, A(end,:) is the last
 
+Remember your ABVs:
+    
+    Always
+    Be
+    Vectorizing
+
+In MATLAB, we shouldn't have to write many for loops. If you're writing a
+for loop, stop and think if there's a different approach. Your code will 
+be 
+ * more readable (MOST IMPORTANT!), as it will be more idiomatic MATLAB, 
+   and make sense to other people who use MATLAB
+ * about 1-1000x times faster. Not only because vectorization is faster, 
+   but for loops in MATLAB are really really really really slow.
+
 %}
+
+
+% look at historical volitlity
+
+
+% calucalte returns
+% preform regression
+
+
+
+% trade signals
+%
+
+
