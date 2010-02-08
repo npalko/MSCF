a66dd26e0fa8c8f539aa9439fafc16fa7b7d07f8
Nicholas Palko <npalko@tepper.cmu.edu>
2010-02-08 15:42:07 -0500

    added initial anova, diagnose

---
+++ +ols/anova.m
@@ -0,0 +1,23 @@
+function anova(f)
+
+
+
+    fprintf('\r')
+    fprintf('Regression ANOVA');
+    fprintf('\r\r')
+
+    fprintf('%6s','Source');
+    fprintf('%10s','df','SS','MS','F','P');
+    fprintf('\r')
+
+    fprintf('%6s','Regr');
+    fprintf('%10.4f',f.dfr,f.ssr,f.ssr/f.dfr,f.f,f.pval);
+    fprintf('\r')
+
+    fprintf('%6s','Resid');
+    fprintf('%10.4f',f.dfe,f.sse,f.sse/f.dfe);
+    fprintf('\r')
+
+    fprintf('%6s','Total');
+    fprintf('%10.4f',f.dfe+f.dfr,f.sse+f.ssr);
+    fprintf('\r')
---
+++ +ols/diagnose.m
@@ -0,0 +1,11 @@
+function diagnose()
+    
+    % Check for the violation of any of the following assumptions
+    %  1. linearity
+    %  2. homoscedasticity
+    %  3. non-normality
+    %  4. independence of errors
+
+
+    
+end
