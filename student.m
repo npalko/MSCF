2abb1e5379896d9d89cd3b4a76b989a1aef14173
Corey M. Hoffstein <corey@hoffstein.com>
2010-02-20 21:59:41 -0500

    Added gradient to regress function

--- student.m
+++ student.m
@@ -1,31 +1,48 @@
 classdef student < handle
    methods (Static)
+               
+       % Solve for the gradient vector
+       function gr = gradient(X,Y,B,df)
+          [n,m] = size(X);
+          r = Y - X*B(1:end-1);
+          
+          z = X;
+          
+          w = r ./ (B(end)^2 * df + r'*r);
+          for i=1:m
+            z(:,i) = z(:,i) .* w;
+          end
+          
+          sder = (-n + (df+1)*sum(r .* w)) / abs(B(end));
+          
+          gr = [(df+1)*sum(z,1) sder];
+       end
        
        % Find the sigma that maximizes the Log Liklihood function given a B
-       function s = findLonS(r,df)
-           n = length(r);
+       function [s, g] = LLH(X,Y,B,df)
+           r = Y - X*B(1:end-1);
            
-           % if x ~ t location, scale distribution with df 
-           % degrees of freedom, then (x-u)/sigma ~ t(df)
-           f = @(s) -sum(log(tpdf(r ./ s, df)));
-           
-           s = fminunc(f, (r'*r)/n);
+           s = -sum(log(tpdf(r ./ B(end), df)));
+           g = -student.gradient(X,Y,B,df);
        end
 
+       % X should contain the intercept vector of 1s
        function B = regress(X,Y,df) 
            [n,m] = size(X);
-
-           bInit = ones(m, 1);
            
-           r = (Y - X*bInit);
-           s = student.findLonS(r, df);
-
-           % if x ~ t location, scale distribution with df 
-           % degrees of freedom, then (x-u)/sigma ~ t(df)
-           f = @(b) -sum(log(tpdf((Y - X*b) ./ s, df)));
+           bInit = X\ Y;
+           r = Y - X*bInit;
            
-           options = optimset('MaxFunEvals', 10000, 'TolX', 1e-16, 'TolFun', 1e-16);
-           [B, fval] = fminunc(f, bInit, options); 
+           bInit = [bInit; sqrt(mean(r .^ 2))];
+
+           fHandle = @(b) student.LLH(X,Y,b,df);
+           
+           options = optimset('MaxFunEvals', 10000, 'TolX', 1e-16, ...
+                              'TolFun', 1e-16, 'GradObj', 'on', ...
+                               'MaxIter', 10000);
+                           
+           [B, fval] = fminunc(fHandle, bInit, options); 
+           B = B(1:end-1);
        end
    end
 end
