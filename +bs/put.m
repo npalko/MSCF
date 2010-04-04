713a031d6edd666b929f9547236e396e693397d1
Nicholas Palko <npalko@tepper.cmu.edu>
2010-04-04 12:41:41 -0400

    added put.price

--- +bs/put.m
+++ +bs/put.m
@@ -2,9 +2,21 @@ classdef put < handle
     methods
         function this = put()
         end
+        function display(this)
+            fprintf('<call>\r');
+        end
+        function disp(this)
+            display(this);
+        end
     end
     methods (Static)
-        function p = price
+        function P = price(S, K, t, vol, r, T, q)
+            
+            if nargin == 6, q = 0; end
+            
+            d1 = bs.d1(S, K, t, vol, r, T, q);
+            d2 = bs.d2(S, K, t, vol, r, T, q);            
+            P = -S*exp(-q*(T-t))*normcdf(-d1) + K*exp(-r*(T-t))*normcdf(-d2);
         end
         function c = charm
         end
