6c37ff2088c67f64d313ba52a3739b40f00b8684
Nicholas Palko <npalko@tepper.cmu.edu>
2010-06-20 20:42:58 -0400

    moved into bsm

--- +bs/d1.m
+++
@@ -1,7 +0,0 @@
-function d1 = d1(S, K, t, vol, r, T, q)
-
-    if nargin == 6, q = 0; end
-	
-	F = S*exp((r-q)*(T-t));
-    d1 = (log(F/K) + ((vol^2)/2)*(T-t)) / (vol * sqrt(T-t));
-end
--- +bs/d2.m
+++
@@ -1,5 +0,0 @@
-function d2 = d2(S, K, t, vol, r, T, q)
-
-    if nargin == 6, q = 0; end
-    d2 = bs.d1(S,K,t,vol,r,T,q) - vol*sqrt(T-t);
-end
